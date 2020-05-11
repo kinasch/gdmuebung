f78f03e04a667662d117b731881229c8978f29ca
kinasch <janik@schoenberg.name>
2020-05-11 16:44:57 +0200

    uebung2

---
+++ src/ueb2/GRDM_U2_S0573689.java
@@ -0,0 +1,186 @@
+package ueb2;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.gui.ImageCanvas;
+import ij.gui.ImageWindow;
+import ij.plugin.PlugIn;
+import ij.process.ImageProcessor;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Panel;
+
+import javax.swing.BorderFactory;
+import javax.swing.JSlider;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+     Opens an image window and adds a panel below the image
+*/
+public class GRDM_U2_S0573689 implements PlugIn {
+
+    ImagePlus imp; // ImagePlus object
+	private int[] origPixels;
+	private int width;
+	private int height;
+	
+	
+    public static void main(String args[]) {
+		//new ImageJ();
+    	//IJ.open("/users/barthel/applications/ImageJ/_images/orchid.jpg");
+    	IJ.open("src\\ueb2\\orchid.jpg");
+		
+		GRDM_U2_S0573689 pw = new GRDM_U2_S0573689();
+		pw.imp = IJ.getImage();
+		pw.run("");
+	}
+    
+    public void run(String arg) {
+    	if (imp==null) 
+    		imp = WindowManager.getCurrentImage();
+        if (imp==null) {
+            return;
+        }
+        CustomCanvas cc = new CustomCanvas(imp);
+        
+        storePixelValues(imp.getProcessor());
+        
+        new CustomWindow(imp, cc);
+    }
+
+
+    private void storePixelValues(ImageProcessor ip) {
+    	width = ip.getWidth();
+		height = ip.getHeight();
+		
+		origPixels = ((int []) ip.getPixels()).clone();
+	}
+
+
+	class CustomCanvas extends ImageCanvas {
+    
+        CustomCanvas(ImagePlus imp) {
+            super(imp);
+        }
+    
+    } // CustomCanvas inner class
+    
+    
+    class CustomWindow extends ImageWindow implements ChangeListener {
+         
+        private JSlider jSliderBrightness;
+		private JSlider jSlider2;
+		private double brightness;
+
+		CustomWindow(ImagePlus imp, ImageCanvas ic) {
+            super(imp, ic);
+            addPanel();
+        }
+    
+        void addPanel() {
+        	//JPanel panel = new JPanel();
+        	Panel panel = new Panel();
+
+            panel.setLayout(new GridLayout(4, 1));
+            jSliderBrightness = makeTitledSilder("Helligkeit", 0, 200, 100);
+            jSlider2 = makeTitledSilder("Slider2-Wert", 0, 100, 50);
+            panel.add(jSliderBrightness);
+            panel.add(jSlider2);
+            
+            add(panel);
+            
+            pack();
+         }
+      
+        private JSlider makeTitledSilder(String string, int minVal, int maxVal, int val) {
+		
+        	JSlider slider = new JSlider(JSlider.HORIZONTAL, minVal, maxVal, val );
+        	Dimension preferredSize = new Dimension(width, 50);
+        	slider.setPreferredSize(preferredSize);
+			TitledBorder tb = new TitledBorder(BorderFactory.createEtchedBorder(), 
+					string, TitledBorder.LEFT, TitledBorder.ABOVE_BOTTOM,
+					new Font("Sans", Font.PLAIN, 11));
+			slider.setBorder(tb);
+			slider.setMajorTickSpacing((maxVal - minVal)/10 );
+			slider.setPaintTicks(true);
+			slider.addChangeListener(this);
+			
+			return slider;
+		}
+        
+        private void setSliderTitle(JSlider slider, String str) {
+			TitledBorder tb = new TitledBorder(BorderFactory.createEtchedBorder(),
+				str, TitledBorder.LEFT, TitledBorder.ABOVE_BOTTOM,
+					new Font("Sans", Font.PLAIN, 11));
+			slider.setBorder(tb);
+		}
+
+		public void stateChanged( ChangeEvent e ){
+			JSlider slider = (JSlider)e.getSource();
+
+			if (slider == jSliderBrightness) {
+				brightness = slider.getValue()-100;
+				String str = "Helligkeit " + brightness; 
+				setSliderTitle(jSliderBrightness, str); 
+			}
+			
+			if (slider == jSlider2) {
+				int value = slider.getValue();
+				String str = "Slider2-Wert " + value; 
+				setSliderTitle(jSlider2, str); 
+			}
+			
+			changePixelValues(imp.getProcessor());
+			
+			imp.updateAndDraw();
+		}
+
+		
+		private void changePixelValues(ImageProcessor ip) {
+			
+			// Array fuer den Zugriff auf die Pixelwerte
+			int[] pixels = (int[])ip.getPixels();
+			
+			for (int y=0; y<height; y++) {
+				for (int x=0; x<width; x++) {
+					int pos = y*width + x;
+					int argb = origPixels[pos];  // Lesen der Originalwerte 
+					
+					int r = (argb >> 16) & 0xff;
+					int g = (argb >>  8) & 0xff;
+					int b =  argb        & 0xff;
+					
+					
+					// anstelle dieser drei Zeilen später hier die Farbtransformation durchführen,
+					// die Y Cb Cr -Werte verändern und dann wieder zurücktransformieren
+					int ly = (int)(0.299*r+0.587*g+0.114*b);
+					int u  = (int)((b-ly)*0.493);
+					int v  = (int)((r-ly)*0.877);
+
+					// TODO alles wird hier verändert (RGB zu YUV erstmal)
+					// Brightness
+					u = (int)(u*((brightness+100)/100));
+					v = (int)(v*((brightness+100)/100));
+					ly = (int)(ly*((brightness+100)/100));
+
+
+					// TODO nach Änderungen wieder zu RGB
+					int rn = (int)(ly+(v/0.877));
+					int bn = (int)(ly+(u/0.493));
+					int gn = (int)((ly/0.587)-((0.299*rn)/0.587)-((0.114*bn)/0.587));
+					if(rn>255){rn=255;}
+					if(bn>255){bn=255;}
+					if(gn>255){gn=255;}
+					// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
+					
+					pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+				}
+			}
+		}
+		
+    } // CustomWindow inner class
+} 
